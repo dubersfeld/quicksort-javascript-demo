f7af95bcda2c84b5ed694ac01be0cef20b5df975
dubersfeld <dominique42ubersfeld@gmail.com>
2017-02-14 16:14:52 +0100

    bad code corrected

--- quicksortDemo.js
+++ quicksortDemo.js
@@ -7,263 +7,220 @@ function canvasSupport() {
 
 function canvasApp() {
 
-  var N = 100;// number of elements to sort
+    var N = 150;// number of elements to sort
+    var delay = 100;
 
-  var delay = 100;
-
-  var randomized = false;
+    var randomized = false;
  
-  var sorter = null;
+    var values = [];// values to be sorted
 
-  var values = [];// values to be sorted
-
-  var results = [];// array of arrays
-
-
-  function Sorter(values) {
+    function Sorter(values) {
    
-    this.values = values;
-    this.aux = [];
-  
-    //results.length = 0;
-    
-    this.swapCount = 0;
-    this.count = 0;
+        this.values = values;
+        this.aux = [];
 
-    // used for animation only
-    this.display = function() {
-        var disp = [];
-        for (var i = 0; i < this.mLength; i++) {
-            disp.push(values[ this.aux[i] ]);
-        }
-        drawValues(disp);
-    };// display
+        this.results = [];
+      
+        this.swapCount = 0;
+        this.count = 0;
 
-    this.mLength = values.length;
+        this.mLength = values.length;
 
-    this.swap = function(i, j) {// swap aux[i] and aux[j], not i an j
-      var temp = this.aux[i];
-      this.aux[i] = this.aux[j];
-      this.aux[j] = temp;
-    };// swap
+        this.swap = function(i, j) {// swap aux[i] and aux[j], not i an j
+            var temp = this.values[i];
+            this.values[i] = this.values[j];
+            this.values[j] = temp;
+        };// swap
 
-    this.partition = function(p, r) {
-        var temp = this.values[ this.aux[r] ];
-        var i = p;
+        this.partition = function(p, r) {
+            var temp = this.values[r];
+            var i = p;
 
-        for (var j = p; j < r; j++) {
-          if (this.values[ this.aux[j] ] <= temp) {
-            i++;
-            this.swap(i-1, j);
-	    results.push(this.aux.slice(0));// save copy for animation after each swap
+            for (var j = p; j < r; j++) {
+                if (this.values[j] <= temp) {
+                    i++;
+                    this.swap(i-1, j);
+                    this.results.push(this.values.slice(0));// save copy for animation after each swap
+                    this.swapCount++;
+                }
+                this.count++;
+            }
+            this.swap(i, r);
+            this.results.push(this.values.slice(0));// save copy for animation after each swap
             this.swapCount++;
-          }
-	  this.count++;
-        }
+          
+            return i; 
+        };// partition
 
-        this.swap(i, r);
-	results.push(this.aux.slice(0));// save copy for animation after each swap
-        this.swapCount++;
+        // main method, non recursive quicksort implementation, using explicit stack
+        this.quickSort = function() {    
+            var stack = [];
 
-        return i; 
-    };// partition
+            this.swapCount = 0;
+            this.count = 0;
 
-    // main method, non recursive quicksort implementation, using explicit stack
-    this.quickSort = function() {
-        
-        var stack = [];
+            var p, q, r, top;
 
-        var p, q, r, top;
-
-        stack.push([0, this.mLength-1]);
+            stack.push([0, this.mLength-1]);
            
-        // main loop
-        while (stack.length > 0) {         
-          top = stack.pop();
-          p = top[0];
-          r = top[1];
+            // main loop
+            while (stack.length > 0) {         
+                top = stack.pop();
+                p = top[0];
+                r = top[1];
                 
-          q = this.partition(p, r);        
+                q = this.partition(p, r);        
 
-          // push on stacks
-          if (q > 0 && p < q-1) {                        
-            stack.push([p, q-1]);
-          }
+                // push on stack
+                if (q > 0 && p < q-1) {                        
+                    stack.push([p, q-1]);
+                }
 
-          if (r < this.mLength && q+1 < r) {                        
-            stack.push([q+1, r]);
-          }
+                if (r < this.mLength && q+1 < r) {                        
+                    stack.push([q+1, r]);
+                }
                  
-        }// while  
+            }
 
-   	results.push(this.aux.slice(0));// save copy for animation      
-
-    };// quickSort
+            this.results.push(this.values.slice(0));// save copy for animation
+        };// quickSort
  
-    // initialize
-    for (var i = 0; i < this.mLength; i++) {
-      this.aux.push(i);
-    } 
-  
-  }// Sorter
+    }// Sorter
 
-  function initGeometry() {
-    var deltaX = (xMax - 20) / N;
+    function initGeometry() {
+        var deltaX = (xMax - 20) / N;
 
-    for (var i = 0; i < N; i++) {
-      xPos.push(10 + i * deltaX);
+        for (var i = 0; i < N; i++) {
+            xPos.push(10 + i * deltaX);
+        }
+    }// initGeometry
+
+
+    function fillBackground() {
+        // draw background
+        context.fillStyle = '#ffffff';
+        context.fillRect(xMin, yMin, xMax, yMax);    
+    }// fillBackground
+
+    function drawValues(values) {
+        fillBackground();
+        for (var i = 0; i < values.length; i++) {
+            context.beginPath();
+            context.strokeStyle = "black";
+            context.lineWidth = 2;
+            context.moveTo(xPos[i], yMax);
+            context.lineTo(xPos[i], yMax - values[i]);
+            context.stroke();
+            context.closePath();
+        }
+    }// drawValues
+
+    if (!canvasSupport()) {
+        return;
+    } else {
+        var theCanvas = $('#canvas')[0];
+        var context = theCanvas.getContext("2d");
     }
-  }// initGeometry
 
+    var xMin = 0;
+    var yMin = 0;
+    var xMax = theCanvas.width;
+    var yMax = theCanvas.height;
 
-  function fillBackground() {
-    // draw background
-    context.fillStyle = '#ffffff';
-    context.fillRect(xMin, yMin, xMax, yMax);    
-  }// fillBackground
-
-  function drawValues(values) {
-    fillBackground();
-    for (var i = 0; i < values.length; i++) {
-      context.beginPath();
-      context.strokeStyle = "black";
-      context.lineWidth = 2;
-      context.moveTo(xPos[i], yMax);
-      context.lineTo(xPos[i], yMax - values[i]);
-      context.stroke();
-      context.closePath();
-    }// for
-  }// drawValues
-
-
-  function renderAnim(aux) {// aux is a copy of Sorter.aux array  
-    fillBackground();
+    var xPos = [];
   
-    var dloc = [];
+    function init(values) {
     
-    for (var i = 0; i < N; i++) {
-      dloc.push(values[ aux[i] ]);
-    }
+        fillBackground();
+        
+	drawValues(values)	
+
+        $('#stanim').find(':submit')[0].disabled = false;
+        $('#initelem').find(':submit')[0].disabled = false;
    
-    drawValues(dloc);
-
-  }// renderAnim
-
-
-  if (!canvasSupport()) {
-    return;
-  } else {
-    var theCanvas = $('#canvas')[0];
-    var context = theCanvas.getContext("2d");
-  }// if
-
- 
-  var xMin = 0;
-  var yMin = 0;
-  var xMax = theCanvas.width;
-  var yMax = theCanvas.height;
-
-  var xPos = [];
+    }// init
   
-  function init() {
-    // reset animation arrays
-    results.length = 0; 
-   
-    if (!randomized) {
-      randomize();
-    }
+    function randomize() {   
+        var val;
+        var more;
 
-    fillBackground();
+        var values = [];
 
-    sorter = null;// reset
-
-    sorter = new Sorter(values);
-
-    // initial display
-    sorter.display();
-
-    sorter.quickSort();
-
-    // display count
-    $('#count').text(sorter.count);
-
-    // display swapCount
-    $('#swapCount').text(sorter.swapCount);
-
-    $('#stanim').find(':submit')[0].disabled = false;
-    $('#initelem').find(':submit')[0].disabled = false;
-  
-  }// init
-  
-  function randomize() { 
-    var val;
-    var more;
-
-    for (var i = 0; i < N; i++) {
-      more = true;// flag
-      while(more) {
-        more = false;
-        val = Math.floor(Math.random() * 500 + 20);
-        for (var j = 0; j < values.length; j++) {
-          if (val == values[j]) {// value already present
-            more = true;
-            break;
-          } 
-        }           
-      }
-      values[i] = val;    
-    }
-
-    randomized = true;
-    init();
-
-  }// randomize
-
-  $('#initelem').submit(function(event) { randomize(); return false; } );
-
-  $('#stanim').submit(function(event) { 
-			startAnim(); 
-			$('#stanim').find(':submit')[0].disabled = true; 
-			$('#initelem').find(':submit')[0].disabled = true;  
-			return false; });
-
-  initGeometry();
-
-  init();
-
- 
-  function startAnim() {
-    // initialize
-    var iAnim = 0;
-    var tempAux = [];
-
-    for (var i = 0; i < values.length; i++) {
-      tempAux.push(i);
-    } 
-
-    function act() {     
-      renderAnim(tempAux);
-
-      tempAux = results[iAnim];
+        for (var i = 0; i < N; i++) {
+              more = true;// flag
+              while(more) {
+                more = false;
+                val = Math.floor(Math.random() * 500 + 20);
+                for (var j = 0; j < values.length; j++) {
+                      if (val == values[j]) {// value already present
+                           more = true;
+                         break;
+                      } 
+                }           
+              }
+              values[i] = val;    
+        }
+     
+        randomized = true;
+        init(values);
     
-      // schedule next step
-      if (iAnim < results.length-1) {
-        iAnim++;
-        setTimeout(act, delay);
-      } else {// animation completed
-  	$('#stanim').find(':submit')[0].disabled = false;
-    	$('#initelem').find(':submit')[0].disabled = false;
-      }
-    }// act
+        return values;  
+    }// randomize
 
-    window.location.hash = "#animanc";
+    $('#initelem').submit(function(event) { randomize(); return false; } );
 
-    act();
+    $('#stanim').submit(function(event) { 
+        startAnim(); 
+        $('#stanim').find(':submit')[0].disabled = true; 
+        $('#initelem').find(':submit')[0].disabled = true;  
+        return false; 
+    });
 
-  }// startAnim
+    function startAnim() {// this is an event handler  
+        var sorter = new Sorter(values);
 
+        sorter.quickSort();
+
+        display(sorter.results, sorter.count, sorter.swapCount);
+    }
+  
+    function display(results, count, swapCount) {
+        // display number of comparisdons
+        $('#countDisp').text(count);
+
+        // display number of swaps
+        $('#swapCountDisp').text(swapCount);
+        
+        // initialize
+        var iAnim = 0;
+        var temp = [];
+
+        function act() {               
+            temp = results[iAnim];
+
+	    drawValues(temp);
+        
+            // schedule next step
+            if (iAnim < results.length-1) {
+              iAnim++;
+              setTimeout(act, delay);
+            } else {// animation completed
+                $('#stanim').find(':submit')[0].disabled = false;
+                $('#initelem').find(':submit')[0].disabled = false;
+            }
+        }// act
+
+        window.location.hash = "#animanc";
+
+        act();
+
+    }
+
+    initGeometry();
+
+    values = randomize();
+    
+    console.log("canvasApp completed");
 }// canvasApp
 
-
 $(document).ready(canvasApp);
-
